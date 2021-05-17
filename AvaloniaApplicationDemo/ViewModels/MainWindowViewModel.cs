a6d2796d4483fee42b201813a6fc4c8cc410b5ae
lupengyao <1147967053@qq.com>
2021-05-17 08:41:39 +0800

    first commit

---
+++ AvaloniaApplicationDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaApplicationDemo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AvaloniaApplicationDemo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaApplicationDemo.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
